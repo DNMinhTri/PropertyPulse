d59937a79dff1379d28709bf1608373df4384483
lucke <luckedao24@gmail.com>
2024-12-03 15:00:28 +0700

    First commit

---
+++ app/layout.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import '@/assets/styles/globals.css';
+
+export const metadata = {
+  title: 'PropertyPulse | Find the Perfect Rental',
+  description: 'Find your dream properties',
+  keywords: 'Rental, Find Rental, Find Homes, Find Properties, No cap'
+}
+
+const MainLayout = ({children}) => {
+  return (
+    <html lang='en'>
+      <body>
+      <div>MainLayout</div>
+      </body>
+    </html>
+  )
+}
+
+export default MainLayout
